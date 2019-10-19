61e33d14e8a27e138532dd964f465de42349235c
Alex Langshur <adlangshur@gmail.com>
2019-10-18 18:45:01 -0700

    Added and cleaned new React application.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import * as serviceWorker from './service-worker';
+import App from './components/app';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+serviceWorker.register();
