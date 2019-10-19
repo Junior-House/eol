67afc8e5ad4ba90b9600c0fa4f3b67ecbda82220
Alex Langshur <adlangshur@gmail.com>
2019-10-18 19:06:06 -0700

    Added navigation structure

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './service-worker';
 import App from './components/app';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
