6ff223f3785c4c22056759f9321df6ff0aae043b
Ryan Othniel Kearns <ryankearns25@gmail.com>
2019-10-20 22:38:55 -0700

    compile import error bugfix

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navigation from "../navigation";
 
 const App = () => (
   <Router>
