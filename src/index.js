5f79940aa146dc1b5865e5b82d477bc72283a6e5
Alex Langshur <adlangshur@gmail.com>
2019-10-21 13:16:59 -0700

    Implemented authentication API

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './service-worker';
-import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.register();
+import App from './components/app';
+import Firebase, { FirebaseContext } from './components/firebase';
+
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>,
+    document.getElementById('root'),
+);
+
+serviceWorker.unregister();
